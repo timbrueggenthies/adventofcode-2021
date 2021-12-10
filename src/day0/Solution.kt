556e7b031888ee5621e8d0d6cd882ecefebd49b8
Tim Brüggenthies <brueggenthies@adesso-mobile.de>
2021-12-10 20:30:44 +0100

    (no branch, rebasing main): main: Day 0 (Blueprint)

---
+++ src/day0/Solution.kt
@@ -0,0 +1,22 @@
+package day0
+
+import readInput
+import readTestInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readTestInput("day0")
+    check(part1(testInput) == 0)
+
+    val input = readInput("day0")
+    println(part1(input))
+    println(part2(input))
+}
